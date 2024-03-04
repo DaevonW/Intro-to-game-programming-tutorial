d0f5d575fcfb7ef00fbf0cc7a0edc3a512fc93aa
DaevonW <158228889+DaevonW@users.noreply.github.com>
2024-03-04 17:59:41 -0500

    Snake tail

--- Snake/Assets/Snake.cs
+++ Snake/Assets/Snake.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Snake : MonoBehaviour
 {
@@ -8,8 +9,13 @@ public class Snake : MonoBehaviour
     //GLOBAL VARIABLES
     Vector3 dir = Vector3.right;
 
-    // Start is called before the first frame update
 
+    //Keep Track of Tail Elements
+    List<Transform> tail = new List<Transform>();
+    bool ate = false;
+    public GameObject tailPrefab;
+
+    // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("MoveSnake", 0.3f, 0.3f);
@@ -22,8 +28,29 @@ public class Snake : MonoBehaviour
     }
     void MoveSnake()
     {
+        Vector3 gap = transform.position;
         transform.Translate(dir); 
 
+        if (ate)
+        {
+            Debug.Log("ate =" + ate);
+            GameObject tailSec = (GameObject)Instantiate(tailPrefab, gap, Quaternion.identity);
+                tail.Insert(0, tailSec.transform);
+
+            ate = false;
+        }
+
+        //check if snake has a tail
+        if (tail.Count > 0)
+        {
+            //move the last Tail to where the Head previously was
+            tail.Last().position = gap;
+
+            //Keep our Tail list in order!
+            //Add Last Tail element to the front of the list and remove from the back 
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
     }
     private void ChangeDirection()
     {
@@ -41,8 +68,10 @@ public class Snake : MonoBehaviour
     {
         if (collision.gameObject.tag == "Food")
         {
+            ate = true;
+
             //Debug.Log("food eaten");
-            Destroy(collision.gameObject);
+            Destroy(collision.gameObject); //Remove the Food
         }
     }
 }
